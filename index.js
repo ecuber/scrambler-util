08dc17b547a447793bd199af93c8ec0a8a1cc025
ecuber <37519236+ecuber@users.noreply.github.com>
2020-05-19 00:16:52 -0400

    fixed path bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const scrambow = new Scrambow();
 const { alias } = require("./util/aliases");
 let scrambles = {};
 
-(function loadFiles(dir = "./scrambles") {
+(function loadFiles(dir = `${__dirname}/scrambles`) {
     fs.readdirSync(dir).map(file => {
         file = file.split(".");
         return file[1] ? scrambles[file[0]] = require(`${dir}/${file[0]}.js`) : loadFiles(`${dir}/${file[0]}`);
