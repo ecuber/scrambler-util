e9b4a5f2947b824ec406a5544402f0b24adbb6ee
ecuber <37519236+ecuber@users.noreply.github.com>
2020-05-24 18:49:33 -0400

    added fmc axis checking

--- scrambles/NNN/333.js
+++ scrambles/NNN/333.js
@@ -1,12 +1,22 @@
 const orient = require("../../util/orient");
+const getScrambles = (cube, amount) => cube.setType("333").get(amount).map(n => n.scramble_string.trim().replace(/ +(?= )/g, ""));
+
 
 module.exports.run = (amount, cube, args) => {
-    let scrambles = cube.setType("333").get(amount).map(n => n.scramble_string.trim().replace(/ +(?= )/g, ""));
+    let scrambles = getScrambles(cube, amount);
     switch (args) {
     case "bld":
         return scrambles.map(s => `${s} ${orient("333")}`);
     case "fmc":
-        return scrambles.map(s => `R' U' F ${s} R' U' F`);
+        scrambles = scrambles.map(s => {
+            let arr = s.split(" ");
+            while (arr[0].startsWith("F") || arr[arr.length - 1].startsWith("R")) {
+                s = getScrambles(cube, 1)[0];
+                arr = s.split(" ");
+            }
+            return `R' U' F ${s} R' U' F`;
+        });
     }
     return scrambles;
 };
+
